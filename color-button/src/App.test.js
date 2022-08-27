9f24e582eb49985907f990745c2f8391eb71aec9
Vikash Kumar <vikash9932@gmail.com>
2022-08-27 20:51:09 +0530

    test: fix test issue eslint warning

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -30,7 +30,7 @@ test('button turns blue when clicked', () => {
   expect(colorButton).toHaveStyle({ backgroundColor: 'MidnightBlue' });
 
   //expect the button text to be 'Change to Midnight Violet Red'
-  expect(colorButton.textContent).toBe('Change to Medium Violet Red');
+  expect(colorButton.toHaveTextContent).toBe('Change to Medium Violet Red');
 });
 
 test('initial conditions', () => {
