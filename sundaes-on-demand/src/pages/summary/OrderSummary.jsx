90e6f3df107ec1d8ab4d3e82f8dd0f1e4814121b
Vikash Kumar <vikash9932@gmail.com>
2022-08-27 23:23:25 +0530

    feat: Summary form

---
+++ sundaes-on-demand/src/pages/summary/OrderSummary.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SummaryForm from './SummaryForm';
+
+const OrderSummary = () => {
+  return (
+    <div>
+      Order summary
+      <SummaryForm />
+    </div>
+  );
+};
+
+export default OrderSummary;
